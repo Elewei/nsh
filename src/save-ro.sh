70e9e110ddaec3e8a0e301148d2dadd7d4dab386
Charlie Root <mzhangqiwei@hotmail.com>
2017-11-27 14:21:41 +0800

    修改Manual

--- src/save-ro.sh
+++ src/save-ro.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#
-# This script is called by NSH when it wants to permanently save the
-# configuration to disk/flash.  This script can be modified by the user
-# to do more if necessary.
-#
 
 if [ ! -f "$1" ]; then
   echo save.sh: not found: $1
@@ -14,5 +9,3 @@ rw
 cp $1 /etc/nshrc
 sync
 ro
-#
-# for flashrd, use save-rw.sh and copy to /flash/nshrc
--- src/save-rw.sh
+++ src/save-rw.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#
-# This script is called by NSH when it wants to permanently save the
-# configuration to disk/flash.  This script can be modified by the user
-# to do more if necessary.
-#
 
 if [ ! -f "$1" ]; then
   echo save.sh: not found: $1
